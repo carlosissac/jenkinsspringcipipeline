8bd1aaf8935f2989a632c613248c867e355e2a74
carlosissac <issac.angulo@gmail.com>
2021-06-08 07:25:42 +0000

    spring initializer

---
+++ src/main/java/com/pipeline/demo/jenkinsspringcipipeline/JenkinsspringcipipelineApplication.java
@@ -0,0 +1,13 @@
+package com.pipeline.demo.jenkinsspringcipipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsspringcipipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsspringcipipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pipeline/demo/jenkinsspringcipipeline/JenkinsspringcipipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipeline.demo.jenkinsspringcipipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsspringcipipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
